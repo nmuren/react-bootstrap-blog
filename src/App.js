e81c957bff7be544abf1e7c8231e7f34174f0fd2
Nebil Muren <nebilmuren@email.com>
2021-06-30 05:50:47 +0300

    done app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import { MainProvider } from "store/MainStore";
 import Routes from "nav/Routes";
 import Header from "views/Header";
 import Footer from "views/Footer";
 
 const App = () => (
-  <MainProvider>
-    <Router>
-      <Header />
-      <Routes />
-      <Footer />
-    </Router>
-  </MainProvider>
+  <Router>
+    <Header />
+    <Routes />
+    <Footer />
+  </Router>
 );
 
 export default App;

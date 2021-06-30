70f60c8393789fcb8ce77593c811e166ea6f3134
Nebil Muren <nebilmuren@gmail.com>
2021-06-30 17:29:21 +0300

    updated pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,43 +1,38 @@
-import React, { useMemo, useState, useEffect } from "react";
+import React, { useMemo } from "react";
 import { keyGenerator } from "utils/commonUtils";
 
 const Pagination = ({ active, total = 0, dataPerPage = 1, onChange }) => {
-  const [items, setItems] = useState([]);
-
   const pageCount = useMemo(
     () => Math.ceil(total / Math.max(1, dataPerPage)),
     [total, dataPerPage]
   );
 
-  useEffect(() => {
-    const listItems = [];
+  const handlePageChange = (event) => {
+    onChange(Number.parseInt(event.target.innerText));
+  };
 
-    const handlePageChange = (event) => {
-      onChange(Number.parseInt(event.target.innerText));
-    };
-
-    for (let i = 1; i <= pageCount; i++) {
-      listItems.push(
-        <button
-          type="button"
-          className="mb-3"
-          onClick={handlePageChange}
-          key={keyGenerator()}
-        >
-          <span
-            className={active === i ? "custom-pagination-active" : "text-muted"}
-            name={i}
+  return (
+    <div className="custom-pagination">
+      {pageCount &&
+        [...Array(pageCount).keys()].map((page) => (
+          <button
+            type="button"
+            className="mb-3"
+            onClick={handlePageChange}
+            key={keyGenerator()}
           >
-            {i}
-          </span>
-        </button>
-      );
-    }
-
-    setItems(listItems);
-  }, [pageCount, active, onChange]);
-
-  return <div className="custom-pagination">{items.map((item) => item)}</div>;
+            <span
+              className={
+                active === page + 1 ? "custom-pagination-active" : "text-muted"
+              }
+              name={page + 1}
+            >
+              {page + 1}
+            </span>
+          </button>
+        ))}
+    </div>
+  );
 };
 
 export default Pagination;
